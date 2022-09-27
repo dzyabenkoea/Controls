8a608e61c6c4828a8f88fc92f9507c811745f2cb
DzybenkoEA <temp@mail.ru>
2022-09-27 17:03:31 +0300

    Добавлен загрузчик картинок

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,12 @@ module.exports={
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use:[
+                    'file-loader'
+                ]
+            }
         ],
     },
 }
