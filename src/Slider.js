d3e24c1fe062db06b97ad747d9897961f1d2268c
DzybenkoEA <temp@mail.ru>
2022-09-27 16:57:38 +0300

    Добавлен модуль слайдера

---
+++ src/Slider.js
@@ -0,0 +1,34 @@
+
+class Slider{
+    constructor() {
+    }
+
+    images = []
+
+    left(){
+
+    }
+
+    right(){
+
+    }
+
+    render(){
+        const div = document.createElement('div')
+        div.classList.add('slider')
+
+        div.innerHTML = `
+        <div class="container">
+            <img src="./images/ahmed-f0S62Iduanw-unsplash.jpg" alt="">
+            <img src="./images/julia-verea-eoi4sNjjAXU-unsplash.jpg" alt="">
+            <img src="./images/martin-bennie-uTEjRNAkGUE-unsplash.jpg" alt="">
+            <img src="./images/taisiia-shestopal-wvfvQ4Uxvu0-unsplash.jpg" alt="">
+        </div>`
+
+        div.textContent = 'Я родился'
+        return div
+    }
+}
+
+export {Slider}
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import {Dropdown} from "./Dropdown";
+import {Slider} from "./Slider";
 
 document.querySelector('.menu').append(Object.create(Dropdown).render())
 document.querySelector('.menu').append(Object.create(Dropdown).render())
+
+document.querySelector('.menu').after(new Slider().render())
