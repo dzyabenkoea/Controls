546b56249c868e13237c4ab2cf8b99ff1c800579
DzybenkoEA <temp@mail.ru>
2022-09-29 08:36:10 +0300

    Изменен вывод компонентов на стартовой странице

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './index.css'
 import {Dropdown} from "./dropdown/dropdown";
 import {Slider} from "./slider/slider";
-
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import {Menu} from "./menu/menu";
 
-document.querySelector('.menu').append(Object.create(Dropdown).render())
-document.querySelector('.menu').append(Object.create(Dropdown).render())
+document.querySelector('#app').append(new Slider().render())
+document.querySelector('#app').append(Object.create(Dropdown).render())
+document.querySelector('#app').before(Object.create(Menu).render())
 
 
-document.querySelector('.menu').after(new Slider().render())
