ae836604e47cea02ed9f4e8d83c4de07042b5b45
DzybenkoEA <temp@mail.ru>
2022-09-29 08:51:55 +0300

    Дропдаун перенесен в меню

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import {Menu} from "./menu/menu";
 
 document.querySelector('#app').append(new Slider().render())
-document.querySelector('#app').append(Object.create(Dropdown).render())
 document.querySelector('#app').before(Object.create(Menu).render())
+
+document.querySelector('.menu-items').append(Object.create(Dropdown).render())
+document.querySelector('.menu-items').append(Object.create(Dropdown).render())
+document.querySelector('.menu-items').append(Object.create(Dropdown).render())
+document.querySelector('.menu-items').append(Object.create(Dropdown).render())
 
 
